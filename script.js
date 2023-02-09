0d028c2e110b162e8cae106fb8f8601ae41cc474
kateFrontend <ecdanilova@gmail.com>
2023-02-09 17:07:07 -0500

    setup script

---
+++ script.js
@@ -0,0 +1,21 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4  // trigger point - where we want to start scrolling
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
